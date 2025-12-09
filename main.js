c3294c25df4358cd4331419b6391d7d6542b0aad
Kayden1236 <tyrellcurrie44@gmail.com>
2025-12-09 11:40:14 -0800

    ORANGE

--- main.js
+++ main.js
@@ -354,6 +354,46 @@ function LoadTargets() {
     }
 }
 
+const Maxorages = 400;
+const Orange_table = [];
+let orages = 0;
+
+function Loadorages() {
+    if (Maxorages > orages) {
+
+        orages+=1;
+
+        const Orange = document.createElement("div");
+        Orange.style.position = "absolute";
+        Orange.style.width = "35px";
+        Orange.style.height = "35px";
+        Orange.health = 75;
+        orages.opacity = "100%";
+       // Target.style.borderRadius = "5%";
+
+        const OrangeDisplayImg = document.createElement("img");
+        OrangeDisplayImg.src = "assets/Orange.png";
+        OrangeDisplayImg.alt = "Orange sprite";
+        OrangeDisplayImg.style.width = "100%";
+        OrangeDisplayImg.style.height = "100%";
+        OrangeDisplayImg.style.display = "pixelated";
+        OrangeDisplayImg.style.userSelect = "none";
+
+        Orange.style.top = Math.random() * (mapHeight - 50) + "px";
+        Orange.style.left = Math.random() * (mapWidth - 50) + "px";
+        
+        Orange_table.push({
+            el: Orange,
+            health: 75,
+            x: parseFloat(Orange.style.left),
+            y: parseFloat(Orange.style.top)
+        });
+
+        Orange.appendChild(OrangeDisplayImg);
+        MapDiv.appendChild(Orange);
+    }
+}
+
 // Player Health Bar (not functional yet)
 let playerHealth = 100;
 let playerMaxHealth = 100;
@@ -477,6 +517,7 @@ buildUpgradeList();
 //update loop
 setInterval(() => {
     LoadTargets();
+    Loadorages();   
     innerBar.style.width = clamp((playerHealth / playerMaxHealth) * 100, 0, 100) + "%";
 }, 100);
 // Initial
